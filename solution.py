7abbf0b33340a47677d0ed666e77ddd7a3d7fad4
ann995 <37317627+ann995@users.noreply.github.com>
2018-10-23 18:40:44 +0200

    Update solution.py
    typo

--- solution.py
+++ solution.py
@@ -26,34 +26,6 @@ def midcircle(size):
     image = np.zeros((size[0],size[1])).astype(np.uint8)
     y,x = np.ogrid[0:size[0], 0:size[1]]
     mask = ((x-x0)**2 + (y-y0)**2) < r**2
-    image[mask] = 255"""
-Rozwiązania do laboratorium 1 z Obrazowania Biomedycznego.
-"""
-import numpy as np
-
-"""
-3 - Kwadrat
-"""
-def square(size, side, start):
-    image = np.zeros((size, size)).astype(np.uint8)
-    image[start[1]:start[1]+side,start[0]:start[0]+side] = 255
-    return image
-
-"""
-3 - Koło
-"""
-def midcircle(size):
-    y0 = size[0]/2
-    x0 = size[1]/2
-
-    if y0<x0:
-    	r = y0/2
-    else:
-    	r = x0/2
-
-    image = np.zeros((size[0],size[1])).astype(np.uint8)
-    y,x = np.ogrid[0:size[0], 0:size[1]]
-    mask = ((x-x0)**2 + (y-y0)**2) < r**2
     image[mask] = 255
     return image
 
@@ -77,42 +49,6 @@ def nn_interpolation(source, new_size):
     #lenna = read_png('data/mono/lenna.png')
     #lenna = np.squeeze(lenna)
 
-    width, height = new_size
-    height_source, width_source = source.shape
-    x = width_source / width
-    y = height_source / height
-    image = np.zeros(new_size).astype(np.uint8)
-    for (i, j) in np.ndindex(image.shape):
-        image[i, j] = source[int(i*y), int(j*x)]
-    return image
-
-
-"""
-5 - Interpolacja dwuliniowa
-"""
-def bilinear_interpolation(source, new_size):
-    pass
-
-    return image
-
-"""
-3 - Szachownica.
-"""
-def checkerboard(size):
-    image = np.zeros((size,size)).astype(np.uint8)
-    side = int(size/8)
-    c = 0
-    for i in range(0, 8):
-        for j in range(0, 8):
-            if ((i+j) % 2 == 1):
-                c = c + 1
-                image[i*side:i*side + side, j*side:j*side + side] = 255
-    return image
-"""
-4 - Interpolacja najbliższych sąsiadów.
-"""
-def nn_interpolation(source, new_size):
-    
     width, height = new_size
     height_source, width_source = source.shape
     x = width_source / width
